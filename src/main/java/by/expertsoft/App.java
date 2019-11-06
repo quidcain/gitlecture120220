ea220d43d9e7a6cb8008398a0f609bc4b1bceee9
Daniel Radezky <daniil.radetsky@expert-soft.by>
2019-11-06 11:05:02 +0300

    Calculate smth

--- src/main/java/by/expertsoft/App.java
+++ src/main/java/by/expertsoft/App.java
@@ -6,8 +6,12 @@ package by.expertsoft;
  */
 public class App 
 {
+    private static int calculateSmth() {
+        return 1000 * 8;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Smth was calculated. Result: " + calculateSmth() );
     }
 }
